3c9db44058a384f652521e3f9d5c651eef2edcbb
Syhans <syhans.aaaaa@gmail.com>
2024-04-20 23:32:55 +0200

    bump

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Calendar } from "@/components/calendar";
 import { getAllEvents } from "@/lib/api";
 
@@ -7,7 +9,9 @@ export default async function Page() {
   return (
     <div className="container mx-auto px-5">
       <div className="h-screen p-10">
-        <Calendar events={events} />
+        <Suspense fallback={null}>
+          <Calendar events={events} />
+        </Suspense>
       </div>
     </div>
   );
