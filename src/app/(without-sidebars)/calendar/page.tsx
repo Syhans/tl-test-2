7ebcad8f082257212c56670b12240cf014aee6a0
Syhans <syhans.aaaaa@gmail.com>
2024-06-03 19:43:08 +0200

    bump

--- src/app/(without-sidebars)/calendar/page.tsx
+++ src/app/(without-sidebars)/calendar/page.tsx
@@ -1,12 +1,15 @@
 import { Calendar } from "@/components/calendar";
 import { getAllEvents } from "@/lib/api";
+import { Suspense } from "react";
 
 export default function Page() {
   const events = getAllEvents();
 
   return (
     <div className="mx-auto h-[calc(100svh-64px)] max-w-[90rem]">
-      <Calendar events={events} />
+      <Suspense fallback={null}>
+        <Calendar events={events} />
+      </Suspense>
     </div>
   );
 }
