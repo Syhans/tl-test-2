e9a764f4618c1ff4480a75ee952d18cab4893d20
Syhans <syhans.aaaaa@gmail.com>
2024-06-06 20:43:32 +0200

    bump

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,12 @@
 import { NextImageWrapper as Image } from "@/components/image";
 import { ImageCarousel } from "@/components/image-carousel";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Lightbulb } from "lucide-react";
 
 export const components = {
-  ImageCarousel,
+  Alert,
+  AlertDescription,
   Image,
+  ImageCarousel,
+  Lightbulb,
 };
