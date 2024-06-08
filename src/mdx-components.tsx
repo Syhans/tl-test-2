253bf01e186c13921c9a52b038ea3ab67e3623dc
Syhans <syhans.aaaaa@gmail.com>
2024-06-08 14:11:16 +0200

    bump

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,13 +1,20 @@
 import { NextImageWrapper as Image } from "@/components/image";
 import { ImageCarousel } from "@/components/image-carousel";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Beer, CircleAlert, CircleHelp, Lightbulb } from "lucide-react";
+import {
+  Beer,
+  Calendar,
+  CircleAlert,
+  CircleHelp,
+  Lightbulb,
+} from "lucide-react";
 
 export const components = {
   Alert,
   AlertTitle,
   AlertDescription,
   Beer: () => <Beer className="h-5 w-5" />,
+  Calendar: () => <Calendar className="h-5 w-5" />,
   CircleAlert: () => <CircleAlert className="h-5 w-5" />,
   CircleHelp: () => <CircleHelp className="h-5 w-5" />,
   Image,
